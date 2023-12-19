d19432fefb827145678e69831cd435d9f3e480e9
Jakub Kaczmarek <jacob_kaczmarek@outlook.com>
2023-12-19 15:48:35 +0100

    Add data transformation pipeline

---
+++ src/mlops_project/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,37 @@
+from mlops_project import logger
+
+from mlops_project.components.data_transfoormation import DataTransformation
+from mlops_project.config.configuration import ConfigurationManager
+
+STAGE_NAME = "Data Transformation Stage"
+
+class DataTransformationPipeline:
+    def __init__(self) -> None:
+        pass
+
+
+    def main(self):
+        config = ConfigurationManager()
+        with open(config.get_data_validation_config().STATUS_FILE) as f:
+            status = f.read()
+
+        if status == "True":
+            try:
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_split()
+            except Exception as e:
+                logger.error(e)
+                raise e
+        else:
+            raise Exception('Invalid data schema')
+        
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+        pipelilne = DataTransformationPipeline()
+        pipelilne.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} complete <<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
