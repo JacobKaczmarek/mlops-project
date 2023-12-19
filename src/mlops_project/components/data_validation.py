0013a42c39ae435b4de9a60ba02039cad5d84244
Jakub Kaczmarek <jacob_kaczmarek@outlook.com>
2023-12-19 15:39:52 +0100

    Add data Validation component

---
+++ src/mlops_project/components/data_validation.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from mlops_project.entity.config_entity import DataValidationConfig
+from mlops_project import logger
+
+
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+
+    def validate_all_columns(self) -> bool:
+        try:
+            validation_status = True
+
+            data = pd.read_csv(self.config.unzip_data_dir)
+            all_cols = list(data.columns)
+            all_schema =  self.config.all_schema.keys()
+
+            for col in all_cols:
+                if col not in all_schema:
+                    validation_status = False
+                    break
+
+            with open(self.config.STATUS_FILE, 'w') as f:
+                f.write(f'Validation status: {validation_status}')
+
+            return validation_status
+
+        except Exception as e:
+            logger.exception(e)
+            raise e
