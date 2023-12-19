775eeb7a787c04ca6ad7501e50eb4f65936988f1
Jakub Kaczmarek <jacob_kaczmarek@outlook.com>
2023-12-19 15:48:54 +0100

    Add pipelines to main file

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from mlops_project import logger
 from mlops_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlops_project.pipeline.stage_02_data_validation import DataValidationPipeline
+from mlops_project.pipeline.stage_03_data_transformation import DataTransformationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
+
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} complete <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    pipelilne = DataValidationPipeline()
+    pipelilne.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} complete <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    pipelilne = DataTransformationPipeline()
+    pipelilne.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} complete <<<<<<<")
 except Exception as e:
     logger.exception(e)
